466485d84c728b9387f1cd45f07d0992f78b8eb4
kadenweaver <kadenpweaver@gmail.com>
2016-11-13 21:20:21 -0500

    what's app

--- app/src/main/java/com/example/yzhan14/soccerkeeper/MainActivity.java
+++ app/src/main/java/com/example/yzhan14/soccerkeeper/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.yzhan14.soccerkeeper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button startbutton = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startbutton = (Button)findViewById(R.id.start_button);
+        startbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(this,StageOneActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
