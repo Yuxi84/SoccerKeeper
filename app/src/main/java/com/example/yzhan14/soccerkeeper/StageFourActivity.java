32425b8323c41daf3d765dfe9dc54a60ec4d2e69
kpweav13 <kadenpweaver@gmail.com>
2016-12-08 09:20:00 -0500

    added stage four

---
+++ app/src/main/java/com/example/yzhan14/soccerkeeper/StageFourActivity.java
@@ -0,0 +1,13 @@
+package com.example.yzhan14.soccerkeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StageFourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stage_four);
+    }
+}
